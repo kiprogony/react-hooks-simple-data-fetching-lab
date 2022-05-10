b07445fc863d5f4c4361dbb1cd50b3c40c9ccb9b
Daryl Rogony <darylrogony@gmail.com>
2022-05-10 11:57:27 +0300

    Completed Assignment fit: fetching data from an API

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, { useState, useEffect }from "react";
+
+
+  function App() {
+    const [dogImage, setDogImage]  = useState(null);
+
+    useEffect(() => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setDogImage(data.message);
+      });
+    }, []);
+
+    if (!dogImage) return <h2>Loading....</h2>
+
+    return <img src={dogImage}  alt="A Random Dog"/>
+  }
+
+  export default App;
